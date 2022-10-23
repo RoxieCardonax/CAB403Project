bc53eaa2dca5c71fffe56a58aec18faabf1e94f2
MichaelG2992 <cab403@cab403vm>
2022-10-24 02:34:19 +1000

    Shared Memory works between processes. Still issue with directly accessing the address by incrementing pointers.

--- shared_mem.h
+++ shared_mem.h
@@ -16,39 +16,49 @@
 #define Max_Per_Level 20 //Number of Max Car Parks
 
 
+//Accessing Shared Memory Segments
+//shm->entry[EntryNum].variable     Where EntryNum is 0-4 for Entry 1-5 respectively
+//shm->exit[ExitNum].variable     Where Exit Num is 0-4 for Exit 1-5 respectively
+//shm->level[LevelNum].variable     Where LevelNum is 0-4 for Level 1-5 respectively
+
+
 
 /**
  * Structs Types for Entries, Exits and Levels Pointers
  */
 typedef struct entrys {
-	pthread_mutex_t *LPR_mutex;
-	pthread_mutex_t *boomgate_mutex;
-	pthread_mutex_t *info_mutex;
-	pthread_cond_t *LPR_cond;
-	pthread_cond_t *boomgate_cond;
-	pthread_cond_t *info_cond;
-	char *lpr[6];
-	char *boomgate;
-	char *display;
+	pthread_mutex_t LPR_mutex;
+	pthread_cond_t LPR_cond;
+	char lpr[6];
+	char padd[2];
+	pthread_mutex_t boomgate_mutex;
+	pthread_cond_t boomgate_cond;
+	char boomgate;
+	pthread_mutex_t info_mutex;
+	pthread_cond_t info_cond;	
+	char display;
 }entrys_t ;
 
 
-typedef struct exits {
-	pthread_mutex_t *LPR_mutex;
-	pthread_mutex_t *boomgate_mutex;
-	pthread_cond_t *LPR_cond;
-	pthread_cond_t *boomgate_cond;
-	char *lpr[6];
-	char *boomgate;
+ typedef struct exits {
+	pthread_mutex_t LPR_mutex;
+	pthread_cond_t LPR_cond;
+	char lpr[6];
+	char padd[2];
+	pthread_mutex_t boomgate_mutex;
+	pthread_cond_t boomgate_cond;
+	char boomgate;	
 } exits_t;
 
 typedef struct levels {
-	pthread_mutex_t *LPR_mutex;
-	pthread_cond_t *LPR_cond;
-	char *lpr[6];
-	char *alarm;
-	int16_t *temp;
-}levels_t ;
+	pthread_mutex_t LPR_mutex;
+	pthread_cond_t LPR_cond;
+	char lpr[6];
+	char padd[2];
+	int16_t temp;	
+	char alarm;
+	char padding[5];
+}levels_t ; 
 
 
 /**
@@ -65,9 +75,9 @@ typedef struct shared_data {
     //Level Struct
     levels_t levels[Num_Of_Level];
 	
-	int fd;
-	char *test;
+	
 } parking_data_t;
+
 
 
 #endif
--- simulator.c
+++ simulator.c
@@ -8,86 +8,85 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include "shared_mem.h"
-#include "memory_layout.h"
+//#include "memory_layout.h"
 
+void testSegment(parking_data_t *shm)
+{
+    printf("SIZE OF  VALUE %ld\n", sizeof(entrys_t));       // Check size of entrys
+    printf("SIZE OF  VALUE %ld\n", sizeof(exits_t));        // Check size of exits
+    printf("SIZE OF  VALUE %ld\n", sizeof(levels_t));       // Check size of levels
+    printf("SIZE OF  VALUE %ld\n", sizeof(*shm));           // Check size of memory segment
+    printf("SIZE OF  VALUE %ld\n", sizeof(shm->entrys[0])); // Check size of memory segment
 
+    // Test Directly Accessing Entrance Boomgate Value to address //DOES NOT WORK CURRENTLY. CANNOT ASSIGN CHAR TO THIS ADDRESS
+    // char *c = (char*)(shm + 184);
+    //*c = 'M';
 
+    // Check Pointers are mapped correctly
+    printf("MEMORY ADDRESS OF SHM + 184: %p\n", (char *)(shm + 184)); // Memory address of Entrance 1 boomgate
 
-//Create Shared Memory segment on startup.
-void* create_shared_memory (parking_data_t *shm)
-{        
-    //Check for previous memory segment.Remove if exits
-    shm_unlink(SHARE_NAME);    
+    // Assign Value to test other process
+    shm->entrys[0].boomgate = 'A';
 
-    //Using share name and both creating and setting to read and write. Read and write for owner, group (0666)
-    //Fail if negative int is returned
-    //Save FD to shared memory segement.
+    while (shm->exits[1].boomgate != 'C') // Wait for Manager to change value
+        sleep;
+
+    printf("SHM VALUE Returned %c\n", shm->entrys[0].boomgate); // Print Value
+}
+
+// Create Shared Memory segment on startup.
+void *create_shared_memory(parking_data_t *shm)
+{
+    int fd;
+    // Check for previous memory segment.Remove if exits
+    shm_unlink(SHARE_NAME);
+
+    // Using share name and both creating and setting to read and write. Read and write for owner, group (0666)
+    // Fail if negative int is returned
+    // Save FD to shared memory segement.
     int open;
-    open = shm_open(SHARE_NAME,O_CREAT | O_RDWR, 0666);
+    open = shm_open(SHARE_NAME, O_CREAT | O_RDWR, 0666);
 
-    if (open <0)
+    if (open < 0)
     {
         printf("Failed to create memory segment");
-        
     }
-    shm->fd = open;
+    fd = open;
 
-    //Configure the size of the memory segment to 2920 bytes
-    if (ftruncate(shm->fd,SHMSZ) == -1)
-    {        
+    // Configure the size of the memory segment to 2920 bytes
+    if (ftruncate(fd, sizeof(parking_data_t)) == -1)
+    {
         printf("Failed to set capacity of memory segment");
-        
     };
-    
-     //Map memory segment to pysical address 
-    shm = mmap(0,SHMSZ, PROT_READ | PROT_WRITE, MAP_SHARED,shm->fd,0);
+
+    // Map memory segment to pysical address
+    shm = mmap(0, sizeof(parking_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
     if (shm == MAP_FAILED)
     {
-         printf("FAILED TO MAP shared memory segment.\n");
+        printf("FAILED TO MAP shared memory segment.\n");
     }
     printf("Created shared memory segment.\n");
-    printf("ADDRESS OF PARKING %p\n",shm);
-    shm->test = (char*)'c';
+    printf("ADDRESS OF PARKING %p\n", shm);
 
     return shm;
-    
 }
 
-
-
 int main()
-{   
-   
-    parking_data_t parking; //Initilize parking segment
-    parking_data_t *shm;    //Holds Mapped Shm Address
+{
 
-    
-    //Map Parking Segment to Memory and retrive address.
-    shm = create_shared_memory(&parking);   
-   
-    //sleep(5);
+    parking_data_t parking; // Initilize parking segment
+    parking_data_t *shm;    // Holds Mapped Shm Address
 
-    //Initilized Pointers to Memory Address of Shared Segment
-     createPointers(shm);    
-     
-    
-     //Check Pointers are mapped correctly     
-    printf("SHM VALUE %p\n",(char*)(shm + 184));   //Should match Entrace Boomgate Value below
-    printf("SHM VALUE %p\n",shm->entrys[0].boomgate); //Print Address
-    printf("SHM VALUE %c\n",shm->entrys[0].boomgate); //Print Value
+    // Map Parking Segment to Memory and retrive address.
+    shm = create_shared_memory(&parking);
 
-   
-    
-    if((munmap (shm, SHMSZ )) ==-1){
-		perror("munmap failed");
-	} 
-	   
-    
-    
+    testSegment(shm);
+
+    if ((munmap(shm, SHMSZ)) == -1)
+    {
+        perror("munmap failed");
+    }
+
     return 0;
-
-
-
-
 }
